366f23a3e981f9ac46eb4b2bbd67fb4148d43354
sergiioo101 <smartros@myuax.com>
2024-04-09 19:03:09 +0200

    test

---
+++ src/test/java/interfaz/EditorTextoFrameTest.java
@@ -0,0 +1,12 @@
+package interfaz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EditorTextoFrameTest {
+
+    @Test
+    void main() {
+    }
+}
